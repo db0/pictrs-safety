e88e8b069ccb93b5c21f2801e9410cb9d110a059
db0 <mail@dbzer0.com>
2023-09-20 11:30:49 +0200

    automatic versioning

--- pictrs_safety_api/__init__.py
+++ pictrs_safety_api/__init__.py
@@ -2,12 +2,12 @@ import os
 import socket
 from uuid import uuid4
 
-from fedi_safety_api.logger import logger
-from fedi_safety_api.flask import APP
-from fedi_safety_api.routes import * 
-from fedi_safety_api.apis import apiv1
-from fedi_safety_api.argparser import args
-from fedi_safety_api.consts import FEDI_SAFETY_VERSION
+from pictrs_safety_api.logger import logger
+from pictrs_safety_api.flask import APP
+from pictrs_safety_api.routes import * 
+from pictrs_safety_api.apis import apiv1
+from pictrs_safety_api.argparser import args
+from pictrs_safety_api.consts import PICTRS_SAFETY_VERSION
 import hashlib
 
 APP.register_blueprint(apiv1)
@@ -18,7 +18,7 @@ def after_request(response):
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE, PATCH"
     response.headers["Access-Control-Allow-Headers"] = "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, apikey, Client-Agent, X-Fields"
-    response.headers["Fedi-Safety-API-Node"] = f"{socket.gethostname()}:{args.port}:{FEDI_SAFETY_VERSION}"
+    response.headers["Fedi-Safety-API-Node"] = f"{socket.gethostname()}:{args.port}:{PICTRS_SAFETY_VERSION}"
     try:
         etag = hashlib.sha1(response.get_data()).hexdigest()
     except RuntimeError:
--- pictrs_safety_api/database/functions.py
+++ pictrs_safety_api/database/functions.py
@@ -5,11 +5,11 @@ from loguru import logger
 from datetime import datetime, timedelta
 from sqlalchemy import func, or_, and_, not_, Boolean
 from sqlalchemy.orm import noload
-from fedi_safety_api.flask import db
-from fedi_safety_api.utils import hash_api_key
+from pictrs_safety_api.flask import db
+from pictrs_safety_api.utils import hash_api_key
 from sqlalchemy.orm import joinedload
-from fedi_safety_api.classes.request import ScanRequest
-from fedi_safety_api import enums
+from pictrs_safety_api.classes.request import ScanRequest
+from pictrs_safety_api import enums
 
 def find_waiting_scan_request():
     query = ScanRequest.query.filter(
